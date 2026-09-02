567fe4ef56c0eff85014a53c8ed82f327a72793b
aicee claire <valledoraiceeclaire@gmail.com>
2026-09-02 18:56:13 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Task Manager</title>
+    @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
