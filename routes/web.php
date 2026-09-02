90304597971c9004c1305455f283335603144151
claireaicee <valledoraiceeclaire@gmail.com>
2026-09-02 16:49:57 +0800

    Modify

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
+use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-// Renders the Inertia page (Tasks/Index.jsx) with the current task list
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
-// Creates a new task
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-
-// Updates a task (used here to toggle is_done)
-Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-
-// Deletes a task
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-
-// Redirect the root URL to the task list for convenience
+// Redirect root to /tasks
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
+
+// Task Routes
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
